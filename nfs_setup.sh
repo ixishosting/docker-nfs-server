8c1d96c8d9aeef5404cf5303a5c5aaa5bda545f6
Andrew Ashcroft <andrew.ashcroft@ixis.co.uk>
2016-03-22 16:17:36 +0000

    Switched to env variable called mounts and removed fsid=0

--- nfs_setup.sh
+++ nfs_setup.sh
@@ -2,11 +2,9 @@
 
 set -e
 
-mounts="${@}"
-
-for mnt in "${mounts[@]}"; do
-  src=$(echo $mnt | awk -F':' '{ print $1 }')
-  echo "$src *(rw,sync,no_subtree_check,fsid=0,no_root_squash)" >> /etc/exports
+env | grep mounts | sed 's/.*=//'  |sed -n 1'p' | tr ',' '\n' | while read mount; do 
+   src=$(echo $mount | awk -F':' '{ print $1 }')
+   echo "$src *(rw,sync,no_subtree_check,no_root_squash)" >> /etc/exports
 done
 
 exec runsvdir /etc/sv
