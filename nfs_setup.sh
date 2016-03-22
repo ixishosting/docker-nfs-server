bf68a673b5197bd8859f8e6cd854496712e3adc9
Andrew Ashcroft <andrew.ashcroft@ixis.co.uk>
2016-03-22 16:58:44 +0000

    no more char issues pls

--- nfs_setup.sh
+++ nfs_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-env | grep mounts | sed 's/.*=//' | sed -n 1'p' | tr ',' '\n' | while read mount; do
+env | grep mounts | sed 's/.*=//' | sed -n 1'p' | tr ',' '\n' | while read mount; do
    echo "$mount *(rw,sync,no_subtree_check,no_root_squash)" >> /etc/exports;
 done
 
