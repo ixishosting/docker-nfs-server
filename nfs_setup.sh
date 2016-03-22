f79ca12ce0d9a4454b97083e0c5d289f1aee378f
Andrew Ashcroft <andrew.ashcroft@ixis.co.uk>
2016-03-22 16:45:33 +0000

    tidy

--- nfs_setup.sh
+++ nfs_setup.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-set -e
 
-env | grep mounts | sed 's/.*=//'  |sed -n 1'p' | tr ',' '\n' | while read mount; do 
-   src=$(echo $mount | awk -F':' '{ print $1 }')
-   echo "$src *(rw,sync,no_subtree_check,no_root_squash)" >> /etc/exports
+env | grep mounts | sed 's/.*=//' | sed -n 1'p' | tr ',' '\n' | while read mount; do
+   echo "$mount *(rw,sync,no_subtree_check,no_root_squash)" >> /etc/exports;
 done
 
 exec runsvdir /etc/sv
