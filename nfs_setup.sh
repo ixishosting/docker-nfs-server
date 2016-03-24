d31a49e69feab9ef3d3338a91fef1ee46ed52251
Andrew Ashcroft <andrew.ashcroft@ixis.co.uk>
2016-03-24 16:57:59 +0000

    testing options

--- nfs_setup.sh
+++ nfs_setup.sh
@@ -2,7 +2,7 @@
 
 
 env | grep mounts | sed 's/.*=//' | sed -n 1'p' | tr ',' '\n' | while read mount; do
-   echo "$mount *(rw,sync,no_subtree_check,no_root_squash)" >> /etc/exports;
+   echo "$mount *(rw,sync,no_subtree_check,no_root_squash,sec=sys,bg,hard,nointr,suid,cio)" >> /etc/exports;
 done
 
 exec runsvdir /etc/sv
