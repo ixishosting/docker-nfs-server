d4242e405c083b94484c50bec558f11d5f0d9f87
Andrew Ashcroft <andrew.ashcroft@ixis.co.uk>
2016-03-29 10:41:28 +0100

    Removed clientside options...

--- nfs_setup.sh
+++ nfs_setup.sh
@@ -2,7 +2,7 @@
 
 echo "/mnt/nfs/ *(rw,sync,no_subtree_check,fsid=0,no_root_squash)" > /etc/exports
 env | grep mounts | sed 's/.*=//' | sed -n 1'p' | tr ',' '\n' | while read mount; do
-   echo "$mount *(rw,sync,no_subtree_check,no_root_squash,sec=sys,hard,nointr,suid,cio)" >> /etc/exports;
+   echo "$mount *(rw,sync,no_subtree_check,no_root_squash,sec=sys)" >> /etc/exports;
 done
 
 exec runsvdir /etc/sv
